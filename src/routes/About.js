9b6635a7a9ab43298c4a4e3a0ee6c7a8f0df53c9
Vikash Singh <vikash@Vikashs-MacBook-Air.local>
2023-05-01 03:45:21 +0530

    Version 1.2

--- src/routes/About.js
+++ src/routes/About.js
@@ -10,18 +10,10 @@ const About= () => {
     <div>
       <Navbar/>
       <HeroImgAbout/>
-      {/* <div className='about_details'> */}
-        {/* <div className='left_about'>
-          <img src={Owner_Pic} alt="Profile_Pic"/>
-        </div>
-        <div className='right_about'>
-          <p>Hi, I'm Vikash Singh</p>
-        </div> */}
-        <div style={{display:"flex", "flex-direction":"row", padding:"3rem"}}>
-          <img src = {Owner_Pic} alt = "Profile_pic" style={{"width":"250px", "border-radius":"50%"}}/>
-          <p style={{margin:'50px'}}>Hi, <br/> I'm Vikash Kumar Singh, born and raised in a small village of Bihar and I have Completed my bachelor's in Computer Science and Engineering (Cyber Security). <br/> I have keen Interest in Python, Java, Machine Learning, Full Stack, Cyber Security and Artificial Intelligence and also I have done Projects on these Skills only.</p>
-        </div> 
-      {/* </div> */}
+      <div style={{display:"flex", "flex-direction":"row", padding:"3rem"}}>
+        <img src = {Owner_Pic} alt = "Profile_pic" style={{"width":"250px", "border-radius":"50%"}}/>
+        <p style={{margin:'50px'}}>Hi, <br/> I'm Vikash Kumar Singh, born and raised in a small village of Bihar and I have Completed my bachelor's in Computer Science and Engineering (Cyber Security) and Currently working in ITC Infotech as a IT Engineer. <br/> I have keen Interest in Python, Java, Machine Learning, Full Stack, Cyber Security and Artificial Intelligence and also I have done Projects on these Skills only.</p>
+      </div> 
       <Footer/>
     </div>
   )
