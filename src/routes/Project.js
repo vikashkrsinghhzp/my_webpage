bdda00f8af118a41405c8af8597c943f8cbba998
Vikash Singh <vikash@Vikashs-MacBook-Air.local>
2023-05-07 00:36:05 +0530

    Version G.Police

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -8,6 +8,7 @@ import Certficate_1 from "../assets/HRC.jpg";
 import Certficate_2 from "../assets/BIG_Data.jpg";
 import Certficate_3 from "../assets/Web_Hacking.jpg";
 import Certficate_4 from "../assets/git.jpg";
+import Certficate_5 from "../assets/Police.jpg";
 
 import Project_1 from "../assets/Proj_1.jpeg";
 import Project_2 from "../assets/City_Conf.png";
@@ -23,6 +24,7 @@ const project = () => {
       <div className='proj-content'>
           <img src={Certficate_4} alt="Certificate 4"/>
           <img src={Certficate_1} alt="Certificate 1"/>
+          <img src={Certficate_5} alt="Certificate 5"/>
           <img src={Certficate_2} alt="Certificate 2"/>
           <img src={Certficate_3} alt="Certificate 3"/>
           
